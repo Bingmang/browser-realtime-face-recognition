7bde00146bb55b22ee4844db8574d33d7d343377
unknown <todd.g@qq.com>
2019-03-28 13:54:39 +0800

    2019.03.28.13.54

---
+++ precache-manifest.75d62d9132e322853b49554252366c97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a75340e83258bfbbae9",
+    "url": "/browser-real-time-face-recognition/static/js/runtime~main.33d209b5.js"
+  },
+  {
+    "revision": "95d2ba214f698c5af273",
+    "url": "/browser-real-time-face-recognition/static/js/main.66204359.chunk.js"
+  },
+  {
+    "revision": "0f9b70f8e69ee06612dc",
+    "url": "/browser-real-time-face-recognition/static/js/2.7efcbb7e.chunk.js"
+  },
+  {
+    "revision": "95d2ba214f698c5af273",
+    "url": "/browser-real-time-face-recognition/static/css/main.65643ce0.chunk.css"
+  },
+  {
+    "revision": "0f9b70f8e69ee06612dc",
+    "url": "/browser-real-time-face-recognition/static/css/2.f009d382.chunk.css"
+  },
+  {
+    "revision": "5171ab8520d8502c5675a51edfe2ffb0",
+    "url": "/browser-real-time-face-recognition/index.html"
+  }
+];
